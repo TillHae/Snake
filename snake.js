189789edc16868be0b1d076b440316f708d45350
tillhauer <t.hauer@fz-juelich.de>
2024-07-30 10:43:54 +0200

    added logic and implemented moveSnake

--- snake.js
+++ snake.js
@@ -7,16 +7,27 @@ const foodColor = "red";
 const SnakeColor = "green"
 
 let apple = (0, 0);     // (x, y)
-let snake = [(7, 4), (6, 4), (5, 4), (3, 4)];
+let snake = [(7, 4), (6, 4), (5, 4), (4, 4)];
+let direction = (-1, 0)  // (left/right, down/up)
+
+// calls changeDirection whenever a key is pressed
+window.addEventListener("keydown", changeDirection())
 
 // start game
 function startGame(){};
+
+// starts next round
+function nextRound(){
+    drawBoard();
+    moveSnake();
+};
 
 // draws the board
 function drawBoard(){
     // set board to black
     board.fillStyle("black");
     board.fillRect(0, 0, gameWidth, gameHeight);
+
     // add Apple
     board.fillStyle(foodColor);
     board.fillRect(apple[0], apple[1], unitSize, unitSize);
@@ -28,17 +39,35 @@ function drawBoard(){
     })
 };
 
+// changes Direction of Snake
+function changeDirection(){
+
+}
+
 // moves Snake
-function moveSnake(){};
+function moveSnake(){
+    snake.push((snake[-1][0] + direction[0], snake[-1][1] + direction[1]));
+    if (gameOver()){
+        endGame();
+    }
+    else{
+        checkApple();
+        nextRound();
+    }
+};
 
 // Test whether game is over
 function gameOver(){};
 
 // checks if apple was eaten // pop from snake when apple was not eaten
-function checkApple(){};
+function checkApple(){
+    eatApple();
+};
 
 // Snake eats the apple
-function eatApple(){};
+function eatApple(){
+    createApple();
+};
 
 // creates an apple at a random position
 function createApple(){};
